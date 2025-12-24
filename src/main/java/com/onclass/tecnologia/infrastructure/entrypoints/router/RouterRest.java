0dc74c543c08ebc9a3bda0a824bab6431e06d090
Ansony <ansonydevx@gmail.com>
2025-12-24 18:46:26 -0500

    refactor: se agregaron mejoras, optimizaciones de flujos para evitar romper la reactividad

---
+++ src/main/java/com/onclass/tecnologia/infrastructure/entrypoints/router/RouterRest.java
@@ -0,0 +1,43 @@
+package com.onclass.tecnologia.infrastructure.entrypoints.router;
+
+import com.onclass.tecnologia.infrastructure.entrypoints.handler.TecnologiaHandler;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterRest {
+
+    @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/tecnologias",
+                    beanClass = TecnologiaHandler.class,
+                    beanMethod = "registrar",
+                    method = org.springframework.web.bind.annotation.RequestMethod.POST
+            ),
+            @RouterOperation(
+                    path = "/tecnologias/exists",
+                    beanClass = TecnologiaHandler.class,
+                    beanMethod = "existen",
+                    method = org.springframework.web.bind.annotation.RequestMethod.POST
+            ),
+            @RouterOperation(
+                    path = "/tecnologias/by-ids",
+                    beanClass = TecnologiaHandler.class,
+                    beanMethod = "obtenerPorIds",
+                    method = org.springframework.web.bind.annotation.RequestMethod.POST
+            )
+    })
+    public RouterFunction<ServerResponse> routerFunction(TecnologiaHandler handler) {
+        return RouterFunctions.route()
+                .POST("/tecnologias", handler::registrar)
+                .POST("/tecnologias/exists", handler::existen)
+                .POST("/tecnologias/by-ids", handler::obtenerPorIds)
+                .build();
+    }
+}
